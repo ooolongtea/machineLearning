83dd9b0ec10c7e5905d9d88a09a8015394c1e7c1
zxy <3409506462@qq.com>
2024-10-29 12:18:35 +0800

    20241029-zxy-实验室:"hw2数据库"

---
+++ src/hw2.py
@@ -0,0 +1,67 @@
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import torch
+import torch.nn as nn
+
+class TIMITDataset(Dataset):
+    def __init__(self, X, y=None):
+        self.data = torch.from_numpy(X).float()
+        if y is not None:
+            y = y.astype(int)
+            self.label = torch.LongTensor(y)#改标签
+        else:
+            self.label = None
+
+    def __getitem__(self, idx):
+        if self.label is not None:
+            return self.data[idx], self.label[idx]
+        else:
+            return self.data[idx]
+
+    def __len__(self):
+        return len(self.data)
+
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.layer1 = nn.Linear(429, 1024)
+        self.layer2 = nn.Linear(1024, 512)
+        self.layer3 = nn.Linear(512, 128)
+        self.out = nn.Linear(128, 39)
+
+        self.act_fn = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.act_fn(x)
+
+        x = self.layer2(x)
+        x = self.act_fn(x)
+
+        x = self.layer3(x)
+        x = self.act_fn(x)
+
+        x = self.out(x)
+
+        return x
+
+data_root='../dataset/timit_11/'
+train = np.load(data_root + 'train_11.npy')
+train_label = np.load(data_root + 'train_label_11.npy')
+test = np.load(data_root + 'test_11.npy')
+
+
+VAL_RATIO = 0.2
+percent = int(train.shape[0] * (1 - VAL_RATIO))
+# 切片 [:percent]是0-percent，区分了训练集和验证集
+train_x, train_y, val_x, val_y = train[:percent], train_label[:percent], train[percent:], train_label[percent:]
+print('Size of training set: {}'.format(train_x.shape))# Size of training set: (983945, 429)表示983945个数据，每个数据的特征值有429个
+print('Size of validation set: {}'.format(val_x.shape))
+
+BATCH_SIZE = 64
+train_set = TIMITDataset(train_x, train_y)
+val_set = TIMITDataset(val_x, val_y)
+train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True) #only shuffle the training data
+val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
+
