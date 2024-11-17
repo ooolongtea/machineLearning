d37176b190aef226d18c8a2aff0087d89d5b0fe2
zhouxingyu <3409506462@qq.com>
2024-11-17 23:57:49 +0800

    20241028zxy-p22

---
+++ hw/5/hw5.py
@@ -0,0 +1,18 @@
+import sys
+import pdb
+import pprint
+import logging
+import os
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils import data
+import numpy as np
+import tqdm.auto as tqdm
+from pathlib import Path
+from argparse import Namespace
+from fairseq import utils
+
+import matplotlib.pyplot as plt
